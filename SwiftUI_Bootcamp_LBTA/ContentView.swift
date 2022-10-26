fee41e14d1e35f002843bb18fba5f0884a14e248
Maxim <ukr-maksim1@yandex.ru>
2022-10-26 12:52:29 +0300

    add project

---
+++ SwiftUI_Bootcamp_LBTA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Bootcamp_LBTA
+//
+//  Created by Maxim Hranchenko on 26.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Bootcamp_LBTA/SwiftUI_Bootcamp_LBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bootcamp_LBTAApp.swift
+//  SwiftUI_Bootcamp_LBTA
+//
+//  Created by Maxim Hranchenko on 26.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bootcamp_LBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
