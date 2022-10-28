f78ff8f3434ab642a7df2802696eaaed814876e3
Maxim <ukr-maksim1@yandex.ru>
2022-10-28 09:09:09 +0300

    geomentryReader

---
+++ SwiftUI_Bootcamp_LBTA/GeometryReaderBootcamp.swift
@@ -0,0 +1,58 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftUI_Bootcamp_LBTA
+//
+//  Created by Maxim Hranchenko on 27.10.2022.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geomentry in
+                        RoundedRectangle(cornerRadius: 20).fill(random())
+                            .shadow(color: random(), radius: 8)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geomentry) * 40),
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+        
+//        GeometryReader { geomentry in
+//            HStack(spacing: 0) {
+//                Rectangle()
+//                    .fill(Color.blue)
+//                    .frame(width: geomentry.size.width * 0.6666)
+//
+//                Rectangle()
+//                    .fill(Color.red)
+//            }
+//            .ignoresSafeArea()
+//        }
+    }
+    
+    func random() -> Color {
+        Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
+    }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - currentX / maxDistance)
+    }
+}
+
+struct GeometryReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderBootcamp()
+    }
+}
